631b4f62d6b768af4a732c0dfc48a3b60efaf815
Carlos Leonardo Rodríguez <aleph0611@gmail.com>
2016-11-07 09:57:26 -0600

    add grid test component

--- bitacoras-cdc-ng2/src/app/test-grid/test-grid.component.ts
+++ bitacoras-cdc-ng2/src/app/test-grid/test-grid.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewContainerRef, trigger, state, style, transition, animate } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MdSnackBar, MdSnackBarConfig } from '@angular/material';
 @Component({
   selector: 'mnx-bitacoras-cdc-test-grid',
@@ -20,12 +21,27 @@ import { MdSnackBar, MdSnackBarConfig } from '@angular/material';
 export class TestGridComponent implements OnInit {
 
   estatus: boolean;
+  formTest: FormGroup;
 
   constructor(private snackBar: MdSnackBar,
-    private viewContainerRef: ViewContainerRef) { }
+    private viewContainerRef: ViewContainerRef,
+    private _builder: FormBuilder) { }
 
   ngOnInit() {
     this.estatus = false;
+    this.formTest = this._builder.group({
+      uno: [null, [Validators.required]],
+      dos: [null, [Validators.required, Validators.maxLength(10)]],
+      tres: [null, [Validators.required, Validators.minLength(5)]]
+    });
+    this.formTest.statusChanges.subscribe(estatus => {
+      console.log('Estatus form', estatus);
+      if (estatus === 'INVALID') {
+        Object.keys(this.formTest.controls).map(prop => this.formTest.controls[prop]).forEach(control => {
+          console.log('errors', JSON.stringify(control.errors));
+        });
+      }
+    });
   }
 
   toogleEstado() {
